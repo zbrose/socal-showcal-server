5d409a145feee0c8304a27dcbb1eacb5a7eaf57b
Zach Brose <97310057+zbrose@users.noreply.github.com>
2025-01-22 17:05:05 -0800

    updates

--- models/index.js
+++ models/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const uri = process.env.MONGO_CONNECTION_STRING;
+const uri = process.env.MONGO_URI;
+if (!uri) {
+  throw new Error("MONGO_URI is not defined in the environment variables");
+}
 //connect to the db uri
 mongoose.connect(uri);
 //grab the db connection
