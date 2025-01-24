b77f4c5164be3baef7ab4bcd37fdbca952ae301b
Zach Brose <97310057+zbrose@users.noreply.github.com>
2025-01-23 20:03:03 -0800

    updates to db

--- testdb.js
+++ testdb.js
@@ -1,40 +1,37 @@
-// const db = require('./models')
-// const { create } = require('./models/event')
+const db = require("./models");
 
+// const { create } = require("./models/event");
 
 // const createEvent = () => {
+//   const newEvent = db.Event.create({
+//     title: "Super Sick Show",
+//     date: Date.now(),
+//     time: "5:15pm",
+//     address: "355 S Cochran",
+//     city: "Los Angeles",
+//     state: "CA",
+//     zipcode: 90036,
+//     cover: 10,
+//     genre: "Folk",
+//     details: "For people who like blake mills",
+//   });
+// };
 
+// createEvent();
 
-//     const newEvent = db.Event.create({
-//         title: 'Super Sick Show',
-//         date: Date.now(),
-//         time: '5:15pm',
-//         address: '355 S Cochran',
-//         city: 'Los Angeles',
-//         state: 'CA',
-//         zipcode: 90036,
-//         cover: 10,
-//         genre: 'Folk',
-//         details: 'For people who like blake mills'
-//     })
-// }
+const User = require("./models/user"); // Import the User model
 
-// createEvent()
-
-
-const db = require('./models')
-
-const testUser = async () => {
+const createUser = async () => {
   try {
-    const newUser = await db.User.create({
-      name: 'Weston',
-      email: 'weston@weston.gov',
-      password: 'password1234'
-    })
-    console.log(newUser)
+    const newUser = await User.create({
+      username: "Weston",
+      email: "weston@weston.gov",
+      password: "password1234",
+    });
+    console.log("New User Created:", newUser);
   } catch (err) {
-    console.log(err)
+    console.error("Error creating user:", err);
   }
-}
+};
 
-testUser()
+createUser();
